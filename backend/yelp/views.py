53b5b0d2a5f8e004c096750f56638d3567530252
Colleen Stewart <collstewart30@gmail.com>
2022-12-05 17:52:46 -0500

    Started logic on POST for yelp app views.py

--- backend/yelp/views.py
+++ backend/yelp/views.py
@@ -7,9 +7,15 @@ from .serializers import YelpSerializer
 from django.shortcuts import get_object_or_404
 
 
+# will need GET for user profile (authenticated): isFavorite, completed, saveCurrent, saveFuture
+# will need POST for user profile (authenticated): save as favorite, mark complete, save for current, save for future
 
-@api_view(['GET'])  
-@permission_classes([AllowAny])
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def get_yelp_favorites(request):
-    
-    return Response('yelp backend')
+
+    if request.method == 'POST':
+        serializer = YelpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)

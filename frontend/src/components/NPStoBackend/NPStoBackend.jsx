c2d78cc44084ac1e7c986611d8b2a1d4425f1442
Colleen Stewart <collstewart30@gmail.com>
2022-12-15 14:12:21 -0500

    Continuing logic on markComplete in NPStoBackend and NPSPage. Installed emailjs-com, created component for emailjs, and imported emailjs and React into component.

--- frontend/src/components/NPStoBackend/NPStoBackend.jsx
+++ frontend/src/components/NPStoBackend/NPStoBackend.jsx
@@ -5,38 +5,38 @@ import useAuth from "../../hooks/useAuth";
 const NPStoBackend = (props) => {
   const [user, token] = useAuth();
 
-  const [event_id, setEvent_id] = useState("");
-  const [park_id, setPark_id] = useState("");
-  const [title, setTitle] = useState("");
-  const [url, setUrl] = useState("");
-  const [image_url, setImage_url] = useState("");
-  const [park_name, setPark_name] = useState("");
-  const [state, setState] = useState("");
-  const [description, setDescription] = useState("");
-  const [type, setType] = useState("");
-  const [saveCurrent, setSaveCurrent] = useState("False");
-  const [saveFuture, setSaveFuture] = useState("False");
+  // const [event_id, setEvent_id] = useState("");
+  // const [park_id, setPark_id] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [url, setUrl] = useState("");
+  // const [image_url, setImage_url] = useState("");
+  // const [park_name, setPark_name] = useState("");
+  // const [state, setState] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [type, setType] = useState("");
+  // const [saveCurrent, setSaveCurrent] = useState("False");
+  // const [saveFuture, setSaveFuture] = useState("False");
   const [completed, setCompleted] = useState("False");
-  const [isFavorite, setIsFavorite] = useState("False");
+  // const [isFavorite, setIsFavorite] = useState("False");
 
-  let eventId = props.event_id;
+  // let eventId = props.event_id;
 
-  let markCompleteNPSData = {
-    user: user.id,
-    event_id: eventId,
-    park_id: park_id,
-    title: title,
-    url: url,
-    image_url: image_url,
-    park_name: park_name,
-    state: state,
-    description: description,
-    type: type,
-    saveCurrent: saveCurrent,
-    saveFuture: saveFuture,
-    completed: completed,
-    isFavorite: isFavorite,
-  };
+  // let markCompleteNPSData = {
+  //   user: user.id,
+  //   event_id: even_id,
+  //   park_id: park_id,
+  //   title: title,
+  //   url: url,
+  //   image_url: image_url,
+  //   park_name: park_name,
+  //   state: state,
+  //   description: description,
+  //   type: type,
+  //   saveCurrent: saveCurrent,
+  //   saveFuture: saveFuture,
+    // completed: completed,
+  //   isFavorite: isFavorite,
+  // };
 
   // URL first in axios. POST and PUT: request body data
 
@@ -44,7 +44,7 @@ const NPStoBackend = (props) => {
     try {
       let response = await axios.post(
         `http://127.0.0.1:8000/api/nps/`,
-        markCompleteNPSData,
+        
         { headers: { Authorization: "Bearer " + token } }
       );
       console.log("NPS backend markComplete updated");
@@ -58,7 +58,7 @@ const NPStoBackend = (props) => {
 
   function handleSubmit(event) {
     event.preventDefault();
-    markComplete(markCompleteNPSData);
+    markComplete();
     console.log("updated NPS markComplete");
     // props.markComplete();
   }
@@ -73,7 +73,7 @@ const NPStoBackend = (props) => {
       onClick={handleSubmit}
     >
       Mark Complete
-      {completed&&"✅"}
+
     </button>
   );
 };
--- frontend/src/pages/NPSPage/NPSPage.jsx
+++ frontend/src/pages/NPSPage/NPSPage.jsx
@@ -4,11 +4,47 @@ import axios from "axios";
 import { npsKEY } from "../../localKey";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import NPStoBackend from "../../components/NPStoBackend/NPStoBackend";
+import AuthContext from "../../context/AuthContext";
+import useAuth from "../../hooks/useAuth";
 
 
 
 const NPSPage = (props) => {
   const [NPSData, setNPSData] = useState([]);
+  const [user, token] = useAuth();
+
+
+  const [event_id, setEvent_id] = useState("");
+  const [park_id, setPark_id] = useState("");
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [image_url, setImage_url] = useState("");
+  const [park_name, setPark_name] = useState("");
+  const [state, setState] = useState("");
+  const [description, setDescription] = useState("");
+  const [type, setType] = useState("");
+  const [saveCurrent, setSaveCurrent] = useState("False");
+  const [saveFuture, setSaveFuture] = useState("False");
+  const [completed, setCompleted] = useState("False");
+  const [isFavorite, setIsFavorite] = useState("False");
+
+
+    let markCompleteNPSData = {
+      user: user.id,
+      event_id: event_id,
+      park_id: park_id,
+      title: title,
+      url: url,
+      image_url: image_url,
+      park_name: park_name,
+      state: state,
+      description: description,
+      type: type,
+      saveCurrent: saveCurrent,
+      saveFuture: saveFuture,
+      completed: completed,
+      isFavorite: isFavorite,
+  };
 
   useEffect(() => {
     let mounted = true;
@@ -31,6 +67,18 @@ const NPSPage = (props) => {
     }
   };
 
+
+  // const markComplete = (NPSData) => {
+  //     setEvent_id(response.data.data.id);
+  //     // setPark_id(response.data.data.);
+  //     setTitle(response.data.data.title);
+  //     setUrl(response.data.data.url);
+  //     setImage_url(response.data.data.images.url);
+  //     setPark_name(response.data.data.relatedParks.fullName);
+  //     setState(response.data.data.relatedParks.states);
+  //     setDescription(response.data.data.shortDescription);
+  // };
+  
   
   return (
     <div>
@@ -51,7 +99,7 @@ const NPSPage = (props) => {
                 frameBorder="0"
               />
             </p>
-            <NPStoBackend markComplete={NPSData} />     
+            {/* <NPStoBackend markComplete={setEvent_id(data.id)} />      */}
           </div>
         ))}
     </div>

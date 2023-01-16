c0eb055319edece57e7dc241ac7d9bdc0592f806
Colleen Stewart <collstewart30@gmail.com>
2023-01-16 16:47:35 -0500

    Added API logos to HomePage

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,6 +2,9 @@ import React from "react";
 import useAuth from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
 import "./HomePage.css";
+import TicketmasterLogo from "../../logo/TicketmasterLogo.png";
+import YelpLogo from "../../logo/YelpLogo.png";
+import NPSLogo from "../../logo/NPSLogo.png";
 
 const HomePage = () => {
   const [user, token] = useAuth();
@@ -14,13 +17,13 @@ const HomePage = () => {
       <h2>Choose from the below options to start planning your date night:</h2>
       <div className="home-grid-container">
         <p>
-          <Link to="/ticketmaster" className="Link">Ticketmaster</Link>
+          <Link to="/ticketmaster" className="Link"><img src={TicketmasterLogo} alt="Ticketmaster"/></Link>
         </p>
         <p>
-          <Link to="/nps" className="Link">NPS</Link>
+          <Link to="/nps" className="Link"><img src={NPSLogo} alt="NPS"/></Link>
         </p>
         <p>
-          <Link to="/yelp" className="Link">Yelp</Link>
+          <Link to="/yelp" className="Link"><img src={YelpLogo} alt="Yelp"/></Link>
         </p>
       </div>
     </div>

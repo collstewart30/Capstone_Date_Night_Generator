a1617aa999d920c6921ec05b974274ac0acf38f5
Colleen Stewart <collstewart30@gmail.com>
2023-03-22 14:21:24 -0400

    Removed ';' from TicketMasterPage divs

--- frontend/src/pages/TicketmasterPage/TicketMasterPage.jsx
+++ frontend/src/pages/TicketmasterPage/TicketMasterPage.jsx
@@ -27,43 +27,39 @@ const TicketMasterPage = () => {
   return (
     <div className="container">
       {/* <h1>TICKETMASTER</h1> */}
-      <img src={TicketmasterLogo2} alt="Ticketmaster"/>
+      <img src={TicketmasterLogo2} alt="Ticketmaster" />
       <h2>Search by state abbreviation:</h2>
-      <SearchBar searchBarParent={getTicketMasterData}/>
+      <SearchBar searchBarParent={getTicketMasterData} />
       <div className="grid-container">
-          {ticketmasterData &&
-            ticketmasterData.map((data) => (
-              <div
-                key={data.id}
-                style={{ border: ".75px solid black", margin: ".5em" }}
-              >
-                <h2>{data.name}</h2>
-                <p>
-                  Location: {data._embedded.venues[0].name}
-                </p>
-                <p>
-                  <img
-                    id="ytplayer"
-                    type="text/html"
-                    width="160"
-                    height="90"
-                    src={data.images[0].url}
-                    border="1px solid #555"
-                  />
-                </p>
-                <TMSaveForFuture
-                  event_id={data.id}
-                  name={data.name}
-                  url={data.url}
-                  image={data.images[0].url}
-                  eventType={data.classifications[0].genre.name}
-                  state={data._embedded.venues[0].state.name}
+        {ticketmasterData &&
+          ticketmasterData.map((data) => (
+            <div
+              key={data.id}
+              style={{ border: ".75px solid black", margin: ".5em" }}
+            >
+              <h2>{data.name}</h2>
+              <p>Location: {data._embedded.venues[0].name}</p>
+              <p>
+                <img
+                  id="ytplayer"
+                  type="text/html"
+                  width="160"
+                  height="90"
+                  src={data.images[0].url}
+                  border="1px solid #555"
                 />
-              </div>
-            ))}
-          ;
-        </div>
-      ;
+              </p>
+              <TMSaveForFuture
+                event_id={data.id}
+                name={data.name}
+                url={data.url}
+                image={data.images[0].url}
+                eventType={data.classifications[0].genre.name}
+                state={data._embedded.venues[0].state.name}
+              />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };

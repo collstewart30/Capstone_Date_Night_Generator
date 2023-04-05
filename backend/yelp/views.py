af3922e37a41515486b431ddb9e51aeb238a7757
Colleen Stewart <collstewart30@gmail.com>
2023-04-05 15:10:12 -0400

    Still attempting Yelp API search feature

--- backend/yelp/views.py
+++ backend/yelp/views.py
@@ -77,18 +77,19 @@ def search_yelp_api(request):
     headers = {'Authorization': 'Bearer {}'.format(api_key)}
     baseUrl = "https://api.yelp.com/v3/businesses/search?term=restaurant&location=Baltimore&limit=1"
 
-    params = {
-        'location': 'Baltimore'
-    }
-    #, params=params
-
     location = request.query_params.get("location")    
     print(location)
+
+    params = {
+        'location': request.query_params.get("location") 
+    }
+    print(params)
+
 
     if location:
         rest_response=rest_response.filter(location__city=location)   #attempting query params from wk9 intermediate django code demo
 
-    rest_response = requests.get("https://api.yelp.com/v3/businesses/search?term=restaurant&limit=12", params, headers=headers)
+    rest_response = requests.get(f"https://api.yelp.com/v3/businesses/search?term=restaurant&limit=12&location={location}",headers=headers)
     # print(rest_response.status_code)
     print(rest_response.headers)
     print(rest_response.json())
